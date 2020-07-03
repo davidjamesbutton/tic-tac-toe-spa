c8e2cbb8d941cae4095f65d7354e97d1609785e2
David Button <davidb@pageuppeople.com>
2020-07-03 16:50:23 +1000

    Updates

---
+++ precache-manifest.de5740fb2a3b8b29607d21dc2bf79e07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e860b715fa66d9b27900160fd23be4d",
+    "url": "/tic-tac-toe-spa/index.html"
+  },
+  {
+    "revision": "a09677db4d7c56f9a595",
+    "url": "/tic-tac-toe-spa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd542167d23932437448",
+    "url": "/tic-tac-toe-spa/static/js/2.9ea58276.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-spa/static/js/2.9ea58276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09677db4d7c56f9a595",
+    "url": "/tic-tac-toe-spa/static/js/main.a65af4dd.chunk.js"
+  },
+  {
+    "revision": "4f01a1e27f18225b60da",
+    "url": "/tic-tac-toe-spa/static/js/runtime-main.c12ec2ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe-spa/static/media/logo.5d5d9eef.svg"
+  }
+]);
